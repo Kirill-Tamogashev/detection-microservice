fix: fix model normalization

detector_app/inference_utils.py
@@ -5,18 +5,20 @@ from io import BytesIO
 import torch
 from torchvision import transforms
 from torchvision.models.detection import (
-    maskrcnn_resnet50_fpn,
-    MaskRCNN_ResNet50_FPN_Weights,
+    maskrcnn_resnet50_fpn_v2,
+    # MaskRCNN_ResNet50_FPN_Weights,
     MaskRCNN_ResNet50_FPN_V2_Weights
 )
 
 
 def configure_inference_setup():
-    model = maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.COCO_V1, rpn_score_thresh=0.75)
+    model = maskrcnn_resnet50_fpn_v2(weights=MaskRCNN_ResNet50_FPN_V2_Weights.COCO_V1, rpn_score_thresh=0.75)
     model.eval()
 
-    to_tensor = transforms.Compose([transforms.PILToTensor()])
-    labels = MaskRCNN_ResNet50_FPN_Weights.COCO_V1.meta['categories']
+    to_tensor = transforms.Compose([
+        transforms.PILToTensor(),
+    ])
+    labels = MaskRCNN_ResNet50_FPN_V2_Weights.COCO_V1.meta['categories']
     return model, to_tensor, labels
 
 
@@ -25,7 +27,7 @@ def infer(url, model, transform, labels):
     image = Image.open(BytesIO(response.content))
     with torch.no_grad():
         tensor = transform(image)
-        tensor = tensor / 255 * 2 - 1
+        tensor = tensor / 255
         result = model([tensor])
 
     return [labels[idx] for idx in result[0]["labels"].tolist()]